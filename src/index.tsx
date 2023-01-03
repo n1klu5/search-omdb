3d536ab69ea1e0d7d026b7be6a1d9346aa03f640
Małgorzata Nikiel <malgorzata.nikiel@leocode.com>
2023-01-03 09:55:40 +0100

    feat: add useful libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import { createRoot } from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
+import { App } from 'components/App';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import { I18NEXT_INSTANCE } from 'shared/i18n';
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
 
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <I18nextProvider i18n={I18NEXT_INSTANCE}>
+      <App />
+    </I18nextProvider>
+  </Provider>,
+);
